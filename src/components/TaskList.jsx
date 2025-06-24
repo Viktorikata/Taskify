2d99a32f72fb6507522da3803511974fc4b69b61
Viktoria <va.2816275@yandex.com>
2025-06-24 10:58:46 +0300

    Добавила компонент TaskList для отображения задач

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,5 +1,17 @@
-function TaskList() {
-  return <div>Форма задачи</div>;
+function TaskList ({tasks}) {
+  if (tasks.length === 0) {
+    return <p>Нет задач</p>
+  }
+
+  return (
+    <ul>
+      {tasks.map((t) =>(
+        <li key={t.id}>
+          {t.title} - <em>{t.category}</em>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default TaskList;
